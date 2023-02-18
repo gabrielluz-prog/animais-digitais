a3d1b963d0485d22dad89723251020057aeab4b7
Gabriel Luz <luz.gabriel010@gmail.com>
2023-02-17 21:34:33 -0300

    Site adicionado

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+function displayAnimals(){
+    var button = document.getElementsByTagName("button")[0]
+    var div = document.getElementById("animals-choices")
+    button.style.display = "none"
+    div.style.display = "block"
+}
